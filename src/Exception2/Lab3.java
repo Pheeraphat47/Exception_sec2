598c57725a0cffe3433da3cd151de7222ae10adf
Pheeraphat Dherachaisuphakij <107838345+Pheeraphat47@users.noreply.github.com>
2023-04-27 09:11:34 +0700

    Add files via upload

---
+++ src/Exception2/Lab3.java
@@ -0,0 +1,26 @@
+
+package Exception2;
+
+public class Lab3 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        try {
+            System.out.println("statement1");
+            System.out.println("statement2");
+            System.out.println("statement3");
+
+        } catch (Exception e1) {
+            
+        } finally {
+            System.out.println("statement4");
+        }
+        System.out.println("statement5");
+        
+        
+    }
+
+    
+}
